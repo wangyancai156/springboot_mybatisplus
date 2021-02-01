7777b26906514de62a2e7605d3f45c50dcb8ce7c
wangyancai666 <ycai@jk-sci.com>
2021-02-01 13:38:05 +0800

    test

--- src/main/java/com/example/springboot_mybatisplus/SpringbootMybatisplusApplication.java
+++ src/main/java/com/example/springboot_mybatisplus/SpringbootMybatisplusApplication.java
@@ -11,5 +11,4 @@ public class SpringbootMybatisplusApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMybatisplusApplication.class, args);
     }
-
 }
